68e3ea474c388487f6ab745e0f0c83eea0c64965
vgalnt <vgal@rambler.ru>
2016-05-21 01:41:18 +0800

    [USBOHCI_NEW] Added new file 'usbohci.h'

---
+++ reactos/drivers/usb/usbohci_new/usbohci.h
@@ -0,0 +1,15 @@
+#ifndef USBOHCI_H__
+#define USBOHCI_H__
+
+#include <ntddk.h>
+#include <windef.h>
+#include <stdio.h>
+#include <wdm.h>
+#include <hubbusif.h>
+#include <usbbusif.h>
+#include <usbdlib.h>
+#include "..\usbmport.h"
+
+
+
+#endif /* USBOHCI_H__ */
