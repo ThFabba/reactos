e6f11e80bea6075b5da68be1bf194ade6e71aa22
vgalnt <vgal@rambler.ru>
2016-05-21 02:35:11 +0800

    [USBOHCI_NEW] DriverEntry function

--- reactos/drivers/usb/usbohci_new/usbohci.c
+++ reactos/drivers/usb/usbohci_new/usbohci.c
@@ -4,4 +4,21 @@
 #include <debug.h>
 
 
+USBPORT_REGISTRATION_PACKET RegPacket;
+
+
+NTSTATUS NTAPI DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+{
+    NTSTATUS Status;
+
+    DPRINT("DriverEntry: DriverObject - %p, RegistryPath - %p\n", DriverObject, RegistryPath);
+    DPRINT("DriverEntry: sizeof(USBPORT_REGISTRATION_PACKET) - %x\n", sizeof(USBPORT_REGISTRATION_PACKET));
+
+    RtlZeroMemory(&RegPacket, sizeof(USBPORT_REGISTRATION_PACKET));
+
+    Status = USBPORT_RegisterUSBPortDriver(DriverObject, 1, &RegPacket);
+    DPRINT("DriverEntry: USBPORT_RegisterUSBPortDriver return Status - %x\n", Status);
+
+    return Status;
+}
 
