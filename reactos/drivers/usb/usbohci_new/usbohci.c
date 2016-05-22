a38945f6fe97cd2d438d11bc49a38c6982de106e
vgalnt <vgal@rambler.ru>
2016-05-22 17:36:41 +0800

    [USBOHCI_NEW] Add stub for OHCI_StartController().

--- reactos/drivers/usb/usbohci_new/usbohci.c
+++ reactos/drivers/usb/usbohci_new/usbohci.c
@@ -7,6 +7,17 @@
 USBPORT_REGISTRATION_PACKET RegPacket;
 
 
+ULONG NTAPI OHCI_StartController(PVOID Context, PUSBPORT_RESOURCES Resources)
+{
+  ULONG                        MiniPortStatus;
+
+  DPRINT("OHCI_StartController: Context - %p, Resources - %p\n", Context, Resources);
+
+  MiniPortStatus = 0;
+
+  return MiniPortStatus;
+}
+
 NTSTATUS NTAPI DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
     NTSTATUS Status;
@@ -18,6 +29,7 @@ NTSTATUS NTAPI DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING Registry
 
     RegPacket.Version                               = 1;                                          // Type: 1 - OHCI, 2 - UHCI, 3 - EHCI
     RegPacket.MiniPortHwResourcesSize               = 0;                                          // Size OHCI MiniPort Extension
+    RegPacket.StartController                       = OHCI_StartController;
 
     Status = USBPORT_RegisterUSBPortDriver(DriverObject, 1, &RegPacket);
     DPRINT("DriverEntry: USBPORT_RegisterUSBPortDriver return Status - %x\n", Status);
