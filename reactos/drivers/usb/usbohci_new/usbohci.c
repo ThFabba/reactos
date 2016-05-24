a6e49e683f71dbdd860d2b5136ce27d6e68e875b
vgalnt <vgal@rambler.ru>
2016-05-25 06:20:04 +0800

    [USBOHCI_NEW] No code. Deleted Spaces.

--- reactos/drivers/usb/usbohci_new/usbohci.c
+++ reactos/drivers/usb/usbohci_new/usbohci.c
@@ -39,7 +39,7 @@ NTSTATUS NTAPI DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING Registry
 
     RegPacket.Version                               = 1;                                          // Type: 1 - OHCI, 2 - UHCI, 3 - EHCI
     RegPacket.MiniPortExtensionSize                 = sizeof(OHCI_EXTENSION);                     // Size OHCI MiniPort Extension
-    RegPacket.MiniPortResourcesSize                 = sizeof(OHCI_HC_RESOURCES);                                          // 
+    RegPacket.MiniPortResourcesSize                 = sizeof(OHCI_HC_RESOURCES);                  // 
     RegPacket.StartController                       = OHCI_StartController;
     RegPacket.EnableInterrupts                      = OHCI_EnableInterrupts;
     RegPacket.DisableInterrupts                     = OHCI_DisableInterrupts;
