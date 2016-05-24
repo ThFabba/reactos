7c99ae431bf4435afddefa3996ca069cef6b8ecc
vgalnt <vgal@rambler.ru>
2016-05-25 06:18:40 +0800

    [USBOHCI_NEW] Replace RegPacket.MiniPortExtensionSize on real value.

--- reactos/drivers/usb/usbohci_new/usbohci.c
+++ reactos/drivers/usb/usbohci_new/usbohci.c
@@ -38,7 +38,7 @@ NTSTATUS NTAPI DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING Registry
     RtlZeroMemory(&RegPacket, sizeof(USBPORT_REGISTRATION_PACKET));
 
     RegPacket.Version                               = 1;                                          // Type: 1 - OHCI, 2 - UHCI, 3 - EHCI
-    RegPacket.MiniPortExtensionSize                 = 0;                                          // Size OHCI MiniPort Extension
+    RegPacket.MiniPortExtensionSize                 = sizeof(OHCI_EXTENSION);                     // Size OHCI MiniPort Extension
     RegPacket.MiniPortResourcesSize                 = sizeof(OHCI_HC_RESOURCES);                                          // 
     RegPacket.StartController                       = OHCI_StartController;
     RegPacket.EnableInterrupts                      = OHCI_EnableInterrupts;
