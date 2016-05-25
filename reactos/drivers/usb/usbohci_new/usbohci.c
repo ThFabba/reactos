584121ed7a2ece98ae32c08f46db076a7bce4d8e
vgalnt <vgal@rambler.ru>
2016-05-26 03:35:21 +0800

    [USBOHCI_NEW] Fill array static EDs for Interrupt Endpoint Schedule. Figure 3-4:  Interrupt ED Structure (OHCI Specification).

--- reactos/drivers/usb/usbohci_new/usbohci.c
+++ reactos/drivers/usb/usbohci_new/usbohci.c
@@ -12,6 +12,9 @@ ULONG NTAPI OHCI_StartController(PVOID Context, PUSBPORT_RESOURCES Resources)
   POHCI_OPERATIONAL_REGISTERS  OperationalRegs;
   POHCI_EXTENSION              OhciExtension = (POHCI_EXTENSION)Context;
   ULONG                        MiniPortStatus = 0;
+  ULONG                        ix;
+  UCHAR                        HeadIndex;
+  POHCI_ENDPOINT_DESCRIPTOR    StaticHwED;
 
   DPRINT("OHCI_StartController: Context - %p, Resources - %p\n", Context, Resources);
 
@@ -31,6 +34,34 @@ ULONG NTAPI OHCI_StartController(PVOID Context, PUSBPORT_RESOURCES Resources)
   OhciExtension->HcResourcesVA = Resources->StartVA;
   OhciExtension->HcResourcesPA = Resources->StartPA;
   DPRINT("OHCI_StartController: HcResourcesVA - %p, HcResourcesPA - %p\n", OhciExtension->HcResourcesVA, OhciExtension->HcResourcesPA);
+
+  for (ix = 0; ix < 63; ix++)  // FIXME 63 == 32+16+8+4+2+1 (Endpoint Poll Interval (ms))
+  {
+    StaticHwED = &OhciExtension->HcResourcesVA->InterrruptHeadED[ix];
+
+    if ( ix == 0 )
+    {
+      HeadIndex = 0;
+      StaticHwED->NextED = 0;
+    }
+    else
+    {
+      HeadIndex = ((ix - 1) >> 1);
+      ASSERT(HeadIndex >= 0 && HeadIndex < 31);
+      StaticHwED->NextED = OhciExtension->IntStaticED[HeadIndex].PhysicalAddress;
+    }
+
+    StaticHwED->EndpointControl.sKip = 1;           // 
+    StaticHwED->TailPointer          = 0;           // 
+    StaticHwED->HeadPointer          = 0;           // 
+
+    OhciExtension->IntStaticED[ix].HwED             = StaticHwED;
+    OhciExtension->IntStaticED[ix].PhysicalAddress  = (ULONG)OhciExtension->HcResourcesPA + sizeof(OHCI_HCCA) + ix * sizeof(OHCI_ENDPOINT_DESCRIPTOR); 
+    OhciExtension->IntStaticED[ix].HeadIndex        = HeadIndex;
+
+    DPRINT("OHCI_StartController: ix - 0x%02X, HeadIndex - 0x%02X, StaticHwED - %p, StaticHwED->NextED - %p\n",
+                                  ix, HeadIndex, StaticHwED, StaticHwED->NextED);
+  }
 
 
   return MiniPortStatus;
