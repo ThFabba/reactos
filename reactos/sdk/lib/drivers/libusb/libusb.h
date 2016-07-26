e23a35f14967e59e1c5560b7fafffe0ce78fb675
vgalnt <vgal@rambler.ru>
2016-07-27 03:37:25 +0800

    [LIBUSB] Define structure _LIBUSB_RH_DESCRIPTORS.

--- reactos/sdk/lib/drivers/libusb/libusb.h
+++ reactos/sdk/lib/drivers/libusb/libusb.h
@@ -55,6 +55,16 @@ typedef struct _WORK_ITEM_DATA
     PRH_INIT_CALLBACK CallbackRoutine;                          // callback routine
 } INIT_ROOT_HUB_CONTEXT, *PINIT_ROOT_HUB_CONTEXT;
 
+typedef struct _LIBUSB_RH_DESCRIPTORS {
+
+  USB_DEVICE_DESCRIPTOR           DeviceDescriptor;             // 18
+  USB_CONFIGURATION_DESCRIPTOR    ConfigDescriptor;             // 9
+  USB_INTERFACE_DESCRIPTOR        InterfaceDescriptor;          // 9
+  USB_ENDPOINT_DESCRIPTOR         EndPointDescriptor;           // 7
+  USB_HUB_DESCRIPTOR              Descriptor;                   // 7 + 2[1..32] (7 + 2..64)
+
+} LIBUSB_RH_DESCRIPTORS, *PLIBUSB_RH_DESCRIPTORS;
+
 //
 // tag for allocations
 //
