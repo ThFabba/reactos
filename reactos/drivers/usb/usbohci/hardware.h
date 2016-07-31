09bcb63d82b51638e2d291387dcfc9ed3cf215fa
vgalnt <vgal@rambler.ru>
2016-07-31 14:11:56 +0800

    [USBOHCI] Add define _OHCI_HC_ENDPOINT_CONTROL. It analog field <Flags> in the _OHCI_ENDPOINT_DESCRIPTOR structure. Name "Flags" - not best name for it member: FunctionAddress or EndpointNumber - it not flag.

--- reactos/drivers/usb/usbohci/hardware.h
+++ reactos/drivers/usb/usbohci/hardware.h
@@ -210,6 +210,23 @@ C_ASSERT(sizeof(OHCIHCCA) == 256);
 #define OHCI_PAGE(x)                ((x) &~ 0xfff)
 #define OHCI_PAGE_OFFSET(x)         ((x) & 0xfff)
 
+//---------------------------------------------------------------------
+typedef union _OHCI_HC_ENDPOINT_CONTROL {
+
+  struct {
+    ULONG  FunctionAddress   : 7;
+    ULONG  EndpointNumber    : 4;
+    ULONG  Direction         : 2;
+    ULONG  Speed             : 1;
+    ULONG  sKip              : 1;
+    ULONG  Format            : 1;
+    ULONG  MaximumPacketSize : 11;
+    ULONG  Reserved          : 5;
+  };
+
+  ULONG  AsULONG;
+
+} OHCI_HC_ENDPOINT_CONTROL, *POHCI_HC_ENDPOINT_CONTROL;
 
 typedef struct _OHCI_ENDPOINT_DESCRIPTOR
 {
